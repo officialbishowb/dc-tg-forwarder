716ccea5d9ab70d1eb1afd3a3008891737814eb4
Bishow Bhattarai <90960311+bbhattarai-tgm@users.noreply.github.com>
2022-03-14 19:46:13 +0100

    File size test

--- bot.py
+++ bot.py
@@ -45,13 +45,12 @@ async def handle_others(message: types.Message):
     fileid,filename=getFileId(message,content_type),getFileName(message,content_type)
     file_object=await bot.get_file(file_id=fileid)
     if(file_object.file_size<8000000):
+        await message.reply("File size is big but trying to download it...")
         await bot.download_file(file_path=file_object.file_path,destination=f"./{filename}") # Download file
         if filename.split(".")[-1] in FILES_EXTENSIONS:
             await message.reply(sendFile_2(filename))
         else:
             await message.reply(sendFile_1(filename))
-    else:
-        await message.reply("File size too big! Aborted.")
     delFile(filename) # Delete the file after sending it
     
 
