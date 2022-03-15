98c8b24cf617e44dd2a430e98797c08c2cc2a096
Bishow Bhattarai <90960311+bbhattarai-tgm@users.noreply.github.com>
2022-03-15 21:30:07 +0100

    Fixed size byte problem

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def handle_others(message: types.Message):
     fileid,filename=getFileId(message,content_type),getFileName(message,content_type)
     file_object=await bot.get_file(file_id=fileid)
 
-    if(file_object.file_size<=80000000):
+    if(file_object.file_size<=8000000):
         await bot.download_file(file_path=file_object.file_path,destination=f"./{filename}") # Download file
         if filename.split(".")[-1] in FILES_EXTENSIONS:
             await message.reply(sendFile_2(filename))
@@ -53,7 +53,7 @@ async def handle_others(message: types.Message):
             await message.reply(sendFile_1(filename))
         delFile(filename) # Delete the file after sending it
 
-    elif(file_object.file_size<=200000000):
+    elif(file_object.file_size<=20000000):
         await message.reply("File size is big but trying to upload to anonfiles and send it...")
         await bot.download_file(file_path=file_object.file_path,destination=f"./{filename}") # Download file
         response=uploadAnonfiles(filename)
